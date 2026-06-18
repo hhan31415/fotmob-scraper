bd9b9981b9f06b607a025aca35c104f8b0a4790c
hhan31415 <hhan31415@gmail.com>
2026-06-18 15:21:26 -0700

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import os
 
 # CHROMEDRIVER_PATH is no longer needed as we use webdriver-manager
-BASE_URL = "https://www.fotmob.com/leagues/47/fixtures/premier-league"
+BASE_URL = "https://www.fotmob.com/leagues/130/fixtures/mls"
+#BASE_URL = "https://www.fotmob.com/leagues/47/fixtures/premier-league"
